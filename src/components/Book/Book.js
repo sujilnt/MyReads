53b87131cb9f6ee360762060ca0eff76f2b9e0e2
root <sujilnt@gmail.com>
2018-10-03 23:53:14 +0000

    removing the html and replacing with book component

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,10 +1,11 @@
-import React ,{Component} from "react";
+import React from "react";
 import BookSelectComp from "../BookSelectComp/BookSelectComp.js";
 const Book=(props)=>{
+  const bookCover =props.bookCover;
  return(
      <div className="book">
                           <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: bookCover }}></div>
                             <BookSelectComp />
                           </div>
                           <div className="book-title">{props.bookTitle}</div>
