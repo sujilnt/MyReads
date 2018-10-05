26f5f73390910e0d8d4afa1dcd79ee07a7f32758
sujilnt <sujilnt@gmail.com>
2018-10-05 17:28:07 +0000

    adding search Bar component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import MyRead from "./components/MyRead/MyRead.js";
+import SearchBar from "./components/SearchBar/SearchBar.js";
+import {Link,Route} from "react-router-dom";
 class BooksApp extends React.Component {
   state = {
     /**
@@ -16,35 +18,11 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
+          	 <Route exact path="/" render={()=>(<MyRead />)} />
+             <div className="open-search">
+              	<Link to='/search'>Add a book</Link>
             </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div>
-          <MyRead />
-           <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-    )}
+           <Route exact path="/search" render={()=>(<SearchBar />)} />
       </div>
 )
   }
