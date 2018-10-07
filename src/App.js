307be90d3186136f931d609ca2859e08e91796bc
sujilnt <sujilnt@gmail.com>
2018-10-07 21:33:11 +0100

    refactroing code #2

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ import './App.css'
 import MyRead from "./components/MyRead/MyRead.js";
 import SearchBar from "./components/SearchBar/SearchBar.js";
 import {Link,Route} from "react-router-dom";
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  };
-  
-  render() {
+const BooksApp =()=> {
     return (
       <div className="app">
           	 <Route exact path="/" render={()=>(
@@ -26,11 +15,10 @@ class BooksApp extends React.Component {
               		<Link to='/search'>Add a book</Link>
 				</div>
             	</div>
-)} />
+            )} />
            <Route exact path="/search" render={()=>(<SearchBar bookApi={BooksAPI} />)} />
       </div>
-)
-  }
-}
+    );
+};
 
 export default BooksApp
