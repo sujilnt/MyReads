067c12ab2ee42a5f98341f1050d32687f628b198
sujilnt <sujilnt@gmail.com>
2018-10-07 17:49:11 +0100

    removing the spaces

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,21 +1,20 @@
 import React from "react";
 import BookSelectComp from "../BookSelectComp/BookSelectComp.js";
 const Book=(props)=>{
-  
-   const {updateFunc,bookCover,id}=props;
- return(
-     <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url( ${bookCover} )`}}></div>
-                            <BookSelectComp 
-									bookupdateFunc={updateFunc}
-									BookId={id} 
-								/>
-                          </div>
-                          <div className="book-title">{props.bookTitle}</div>
-                          <div className="book-authors">{props.bookAuthor}</div>
-                        </div>
+    const {updateFunc,bookCover,id}=props;
+    return(
+        <div className="book">
+             <div className="book-top">
+                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url( ${bookCover} )`}}></div>
+                    <BookSelectComp
+					    bookupdateFunc={updateFunc}
+					    BookId={id}
+                    />
+                </div>
+             <div className="book-title">{props.bookTitle}</div>
+             <div className="book-authors">{props.bookAuthor}</div>
+     </div>
  );
-}
+};
 
 export default Book;
